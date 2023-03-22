7d138858a0c1a0e039c1683210ab956bd1841d9b
鴨嘴獸(Lzt) <68383526+lzt0104@users.noreply.github.com>
2023-03-23 01:00:50 +0800

    0323

---
+++ 20230316week4HWQ16-2.py
@@ -0,0 +1,18 @@
+class NoteBook:
+    def __init__(self):
+        self.CPU = CPU()
+        self.screen = Screen()
+        self.kb = Kb()
+        self.disk = Disk()
+
+class CPU:
+    pass
+
+class Screen:
+    pass
+
+class Kb:
+    pass
+
+class Disk:
+    pass
